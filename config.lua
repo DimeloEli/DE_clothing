5be0b4b9c9f0883c53777b3eda276df6de7b6140
DimeloEli <108193709+DimeloEli@users.noreply.github.com>
2022-11-25 01:51:27 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,62 @@
+Config = {}
+
+Config.Shirt = {
+    Male = {
+        Torso = 15,
+        Shirt = 15
+    },
+    Female = {
+        Torso = 15,
+        Shirt = 15
+    }
+}
+
+Config.Undershirt = {
+    Male = 15,
+    Female = 14
+}
+
+Config.Bra = {
+    Off = 82, -- The shirtless slot for when doing /bra off (Females only.)
+    On = 15 -- The slot for when doing /bra on (Females only.)
+}
+
+Config.Panties = {
+    Off = 15, -- The slot for the /panties off command (Females only.)
+    On = 17 -- The slot for the /panties on command (Females only.)
+}
+
+Config.Pants = {
+    Male = 22,
+    Female = 17
+}
+
+Config.Shoes = {
+    Male = 34,
+    Female = 35
+}
+
+Config.Chain = {
+    Male = 0,
+    Female = 0
+}
+
+Config.Decals = {
+    Male = 0,
+    Female = 0
+}
+
+Config.Vest = {
+    Male = 0,
+    Female = 0
+}
+
+Config.Bags = {
+    Male = 0,
+    Female = 0
+}
+
+Config.Mask = {
+    Male = 0,
+    Female = 0
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'DimeloEli'
+description 'Clothing Commands Script Inspired by Gta World'
+
+client_scripts {
+    'client/*.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
